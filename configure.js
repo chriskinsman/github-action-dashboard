7e154608aaf57b651655b2d7868b721e71f1fa12
Chris Kinsman <chris@kinsman.net>
2021-11-27 16:40:28 -0800

    Prevent routes from loading during build

--- configure.js
+++ configure.js
@@ -1,29 +1,32 @@
-const path = require('path');
-const runStatus = require('./runstatus');
+const path = require("path");
+const runStatus = require("./runstatus");
 
 // Handle when server is started from vue-cli vs root
-if (path.basename(process.cwd()) === 'client') {
-    require('dotenv').config({ path: path.resolve(process.cwd(), '../.env') });
+if (path.basename(process.cwd()) === "client") {
+  require("dotenv").config({ path: path.resolve(process.cwd(), "../.env") });
+} else {
+  require("dotenv").config();
 }
-else {
-    require('dotenv').config()
-}
-const debug = require('debug')('action-dashboard:configure');
+const debug = require("debug")("action-dashboard:configure");
 //debug(`environment`, process.env);
 
-const bodyParser = require('body-parser')
-const routes = require('./routes')
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 module.exports = {
-    before: (app) => {
-        app.use(bodyParser.json());
-        app.use('/api', routes);
-    },
-    after: (app, server) => {
-        // Attach socket.io to server
-        runStatus.init(server);
+  before: (app) => {
+    if (process.env.DOCKER_BUILD !== "TRUE") {
+      app.use(bodyParser.json());
+      app.use("/api", routes);
     }
-}
+  },
+  after: (app, server) => {
+    if (process.env.DOCKER_BUILD !== "TRUE") {
+      // Attach socket.io to server
+      runStatus.init(server);
+    }
+  },
+};
 
 // Loads webhook support if GITHUB_APP_WEBHOOK_SECRET defined
-const webhooks = require('./webhooks');
+const webhooks = require("./webhooks");
